96cdd828944c4799487e8e597e80d4ad992ad50a
kaguya <1435608435@qq.com>
2024-12-04 21:09:36 +0800

    🐛fix: language display issue

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -6,15 +6,15 @@ export default defineUserConfig({
   base: "/",
 
   locales: {
-    "/en/": {
-      lang: "en-US",
-      title: "Kaguya233qwq's Blog",
-      description: "A blog of kaguya233qwq",
-    },
     "/": {
       lang: "zh-CN",
       title: "沉默的子龙君的博客",
       description: "这是沉默的子龙君的博客",
+    },
+    "/en/": {
+      lang: "en-US",
+      title: "Kaguya233qwq's Blog",
+      description: "A blog of kaguya233qwq",
     },
   },
 
