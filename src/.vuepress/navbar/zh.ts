10de02d8bf49d47d4e4dddaf3f24a75a6421c36d
marisa <1435608435@qq.com>
2024-12-17 14:58:29 +0800

    📖update docs

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -3,8 +3,8 @@ import { navbar } from "vuepress-theme-hope";
 export const zhNavbar = navbar([
   "/",
   {
-    text: "画廊",    // 菜单项名称
-    icon: "file",   // 图标
+    text: "画廊",
+    icon: "file",
     link: "/gallery/",
   },
 ]);
