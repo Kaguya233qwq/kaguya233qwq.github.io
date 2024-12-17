7b4e59023407f96a6cbae18cf5335c098f63f589
marisa <1435608435@qq.com>
2024-12-17 14:52:08 +0800

    🐛fix route issue

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -3,10 +3,8 @@ import { navbar } from "vuepress-theme-hope";
 export const zhNavbar = navbar([
   "/",
   {
-    text: "画廊",
-    icon: "file",
-    prefix: "/gallery/",
-    children: [
-    ],
-  }
+    text: "画廊",    // 菜单项名称
+    icon: "file",   // 图标
+    link: "/gallery/",
+  },
 ]);
